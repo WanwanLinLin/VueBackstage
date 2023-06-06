da605038cb34c4ae3337a9b99e3b45982347de4f
WanwanLinLin <2283816735@qq.com>
2023-06-06 21:36:30 +0800

    add: 新增添加商品类目接口

--- src/api/product/attr.js
+++ src/api/product/attr.js
@@ -1,13 +1,16 @@
 // 品牌属性管理
 import request from "@/utils/request"
 
+// 增加商品分类的接口
+export const reqNewCategory = (category1Id, category2Id, category3Id)=>request({url: "/admin/categoryManagement/addCategory", method: "get", params: {category1Id, category2Id, category3Id}});
+
 // 获取一级分类数据的接口
 export const reqCategory1List = ()=>request({url: "/admin/categoryManagement/getCategory1", method: "get"});
 
 // 获取二级分类数据的接口
 export const reqCategory2List = (category1Id)=>request({url: `/admin/categoryManagement/getCategory2/${category1Id}`, method: "get"});
 
-//获取三级分类数据的接口
+// 获取三级分类数据的接口
 export const reqCategory3List = (category2Id)=>request({url: `/admin/categoryManagement/getCategory3/${category2Id}`, method: "get"});
 
 // 获取平台属性的接口
--- src/views/product/AddCategory/index.vue
+++ src/views/product/AddCategory/index.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
+    <el-card style="margin: 20px 0px;">
+      <CategorySelect @getCategoryId="getCategoryId"></CategorySelect>
+    </el-card>
+    <br>
     <el-input v-model="input1" placeholder="请输入一级类目名称"></el-input>
     <br><br>
-    <el-input v-model="input2" placeholder="请输入二级类目名称"></el-input>
+    <el-input v-show="input1" v-model="input2" placeholder="请输入二级类目名称"></el-input>
     <br><br>
-    <el-input v-model="input3" placeholder="请输入三级类目名称"></el-input>
+    <el-input v-show="input2" v-model="input3" placeholder="请输入三级类目名称"></el-input>
     <br><br>
-    <el-button type="primary" @click="submitForm">提交</el-button>
+    <el-button v-show="input1" type="primary" @click="submitForm">提交</el-button>
   </div>
 </template>
 
@@ -22,9 +26,19 @@ export default {
     };
   },
   methods: {
-    submitForm() {
+    async submitForm() {
       // 这里是提交表单的逻辑
-      console.log('提交表单');
+      let result = await this.$API.attr.reqNewCategory(this.input1, this.input2, this.input3);
+      if (result.code == 200) {
+        // console.log(result.message)
+        this.$message({
+        message: result.message,
+        type: 'success'
+      })
+      } else {
+        console.log(result.message)
+      }
+      console.log(this.input1, this.input2, this.input3);
     }
   }
 };
